c943da7f2d8d9cb40b46a1701e8a6b9c4a88ded8
Hugo Correia <hfcorreia91@gmail.com>
2013-03-24 14:58:32 +0000

    Added Util with read and write file operations

---
+++ CommonTypes/Util.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace CommonTypes
+{
+    public static class Util
+    {
+        public static void createDir(String dir)
+        {
+            if (!System.IO.File.Exists(dir))
+            {
+                System.IO.Directory.CreateDirectory(dir);
+            }
+        }
+
+        public static void writeToDisk(File file, String clientName)
+        {
+            string dirName = Properties.Resources.TEMP_DIR + "\\" + clientName;
+            Util.createDir(dirName);
+
+            System.Xml.Serialization.XmlSerializer writer =
+            new System.Xml.Serialization.XmlSerializer(typeof(File));
+
+            System.IO.StreamWriter fileWriter = new System.IO.StreamWriter(
+             @dirName + getFileName(file.Name, file.Version) );
+            Console.WriteLine(file.Name);
+            Console.ReadLine();
+            writer.Serialize(fileWriter, file);
+            fileWriter.Close();
+        }
+
+        public static File readFromDisk(String clientName, String name, Int32 version)
+        {
+            System.Xml.Serialization.XmlSerializer reader =
+        new System.Xml.Serialization.XmlSerializer(typeof(File));
+
+            string dirName = Properties.Resources.TEMP_DIR + "\\" + clientName + getFileName(name, version) ;
+            System.IO.StreamReader fileReader = new System.IO.StreamReader(dirName);
+            
+            File file = new File();
+            file = (File)reader.Deserialize(fileReader);
+
+            return file;
+        }
+
+        public static String getFileName(String name, Int32 version)
+        {
+            return "\\" + name + "-V" + version + ".xml";
+        }
+    }
+}
