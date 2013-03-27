8f31fcdc7e098d55d60e1e5ea23cea9025e4e670
Hugo Correia <hfcorreia91@gmail.com>
2013-03-27 15:50:57 +0000

    Renamed CommonTypes/RemoteObjectWrapper.cs to CommonTypes/ServerObjectWrapper.cs

---
+++ CommonTypes/ServerObjectWrapper.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace CommonTypes
+{
+    [Serializable]
+    public class ServerObjectWrapper
+    {
+        
+        public int Port { get; set; }
+        public int Id { get; set; }
+        public string Host { get; set; }
+        public string URL { get { return "tcp://" + Host + ":" + Port + "/" + Id; } }
+
+        private Object wrappedObject = null;
+
+        public ServerObjectWrapper(ServerObjectWrapper original) : this(original.Port, original.Id, original.Host) { }
+
+        public ServerObjectWrapper( int port, int id, string host ) 
+        {
+            Port = port;
+            Id = id;
+            Host = host;
+        }
+
+        public T getObject<T>() 
+        {
+            if ( wrappedObject == null )
+            {
+                wrappedObject = Activator.GetObject( typeof(T), URL );
+            }
+
+            return (T)wrappedObject;
+        }
+    }
+}
