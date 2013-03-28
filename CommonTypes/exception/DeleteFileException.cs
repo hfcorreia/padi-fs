4170950bfd80a5f031f3fd3cd3076da30b25e4c2
Hugo Correia <hfcorreia91@gmail.com>
2013-03-28 13:38:32 +0000

    Delete now throws exceptions

---
+++ CommonTypes/exception/DeleteFileException.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace CommonTypes {
+
+    namespace Exceptions
+    {
+        [Serializable]
+        public class DeleteFileException : ApplicationException
+        {
+
+            public DeleteFileException(String message) : base(message) { }
+            public DeleteFileException(System.Runtime.Serialization.SerializationInfo info, System.Runtime.Serialization.StreamingContext contex)
+                : base(info, contex)
+            {
+            }
+        }
+    }
+}
