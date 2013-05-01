58784c0f4f669088fe1a0782cd4f24508719dd49
pedrorijo91 <pedrorijo91@gmail.com>
2013-05-01 01:05:54 +0100

    Delete operation

---
+++ MetaDataServer/MetaDataLog.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace MetaDataServer
+{
+    class MetaDataLog
+    {
+        private List<MetaDataOperation> log = new List<MetaDataOperation>();
+        private MetaDataServer Md { get; set; }
+
+        public MetaDataLog(MetaDataServer md)
+        {
+            Md = md;
+        }
+
+        public void registerOperation(MetaDataOperation op)
+        {
+            log.Add(op);
+        }
+
+        public void printLog()
+        {
+            Console.WriteLine("Log from MD " + Md.Id + " com " + log.Count + " operacoes registadas");
+            foreach(MetaDataOperation op in log)
+            {
+                Console.WriteLine("Operation: " + op);
+            }
+        }
+
+    }
+}
---
+++ MetaDataServer/operation/MetaDataDeleteOperation.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using CommonTypes.Exceptions;
+using CommonTypes;
+using System.Threading;
+
+namespace MetaDataServer
+{
+    class MetaDataDeleteOperation : MetaDataOperation
+    {
+
+        String ClientID {get;set;}
+        String Filename { get; set; }
+
+        public FileMetadata Result { get; set; }
+
+
+        public MetaDataDeleteOperation(String clientID, string filename)
+        {
+            ClientID = clientID;
+            Filename = filename;
+        }
+
+        public override void execute(MetaDataServer md)
+        {
+            if (!md.FileMetadata.ContainsKey(Filename))
+            {
+            throw new CommonTypes.Exceptions.DeleteFileException("#MDS.delete - File " + Filename + " does not exist");
+            }
+
+            md.FileMetadata.Remove(Filename);
+            Console.WriteLine("#MDS: Deleted file: " + Filename);
+            md.makeCheckpoint();
+        }      
+    }
+}
